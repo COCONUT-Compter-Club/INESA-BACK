bb66f02e4fb1f6445caccbd9eced571c2b5b2b66
Kiki012309 <rezkyamaliahrusli@gmail.com>
2025-05-25 18:54:09 +0800

    update sekretaris

--- Sekretaris_Backend/controller/suratkeluar_controller.go
+++ Sekretaris_Backend/controller/suratkeluar_controller.go
@@ -1,129 +1,117 @@
 package controller
 
 import (
- "Sekertaris/model"
- "Sekertaris/service"
- "encoding/json"
- "io"
- "net/http"
- "os"
- "path/filepath"
- "strconv"
+	"Sekertaris/model"
+	"Sekertaris/service"
+	"encoding/json"
+	"io"
+	"net/http"
+	"strconv"
 
- "github.com/julienschmidt/httprouter"
+	"github.com/julienschmidt/httprouter"
 )
 
 type SuratKeluarController struct {
- service *service.SuratKeluarService
+	service *service.SuratKeluarService
 }
 
 func NewSuratKeluarController(service *service.SuratKeluarService) *SuratKeluarController {
- return &SuratKeluarController{service: service}
+	return &SuratKeluarController{service: service}
 }
 
 func (c *SuratKeluarController) AddSuratKeluar(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
- r.ParseMultipartForm(10 << 20)
+	r.ParseMultipartForm(10 << 20)
 
- surat := &model.SuratKeluar{
-  Nomor:     r.FormValue("nomor"),
-  Tanggal:   r.FormValue("tanggal"),
-  Perihal:   r.FormValue("perihal"),
-  Ditujukan: r.FormValue("ditujukan"),
-  Title:     r.FormValue("title"),
- }
+	surat := &model.SuratKeluar{
+		Nomor:     r.FormValue("nomor"),
+		Tanggal:   r.FormValue("tanggal"),
+		Perihal:   r.FormValue("perihal"),
+		Ditujukan: r.FormValue("ditujukan"),
+		Title:     r.FormValue("title"),
+	}
 
- file, fileHeader, err := r.FormFile("file")
- if err != nil {
-  http.Error(w, "File wajib diupload", http.StatusBadRequest)
-  return
- }
- defer file.Close()
+	file, fileHeader, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, "File wajib diupload", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
 
- err = c.service.AddSuratKeluar(surat, file, fileHeader.Filename)
- if err != nil {
-  http.Error(w, err.Error(), http.StatusBadRequest)
-  return
- }
+	err = c.service.AddSuratKeluar(surat, file, fileHeader.Filename)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
- w.WriteHeader(http.StatusCreated)
- json.NewEncoder(w).Encode(map[string]string{"message": "Surat keluar berhasil ditambahkan"})
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(map[string]string{"message": "Surat keluar berhasil ditambahkan"})
 }
 
 func (c *SuratKeluarController) GetAllSuratKeluar(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
- surat, err := c.service.GetAllSuratKeluar()
- if err != nil {
-  http.Error(w, "Gagal mengambil data", http.StatusInternalServerError)
-  return
- }
+	surat, err := c.service.GetAllSuratKeluar()
+	if err != nil {
+		http.Error(w, "Gagal mengambil data", http.StatusInternalServerError)
+		return
+	}
 
- w.Header().Set("Content-Type", "application/json")
- json.NewEncoder(w).Encode(surat)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(surat)
 }
 
 func (c *SuratKeluarController) GetSuratKeluarById(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
- id, _ := strconv.Atoi(ps.ByName("id"))
+	id, _ := strconv.Atoi(ps.ByName("id"))
 
- surat, err := c.service.GetSuratKeluarById(id)
- if err != nil {
-  http.Error(w, "Data tidak ditemukan", http.StatusNotFound)
-  return
- }
+	surat, err := c.service.GetSuratKeluarById(id)
+	if err != nil {
+		http.Error(w, "Data tidak ditemukan", http.StatusNotFound)
+		return
+	}
 
- w.Header().Set("Content-Type", "application/json")
- json.NewEncoder(w).Encode(surat)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(surat)
 }
 
 func (c *SuratKeluarController) UpdateSuratKeluarByID(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
- id, _ := strconv.Atoi(ps.ByName("id"))
+	id, _ := strconv.Atoi(ps.ByName("id"))
 
- r.ParseMultipartForm(10 << 20)
+	r.ParseMultipartForm(10 << 20)
 
- surat := &model.SuratKeluar{
-  Nomor:     r.FormValue("nomor"),
-  Tanggal:   r.FormValue("tanggal"),
-  Perihal:   r.FormValue("perihal"),
-  Ditujukan: r.FormValue("ditujukan"),
-  Title:     r.FormValue("title"),
-  File:      r.FormValue("existing_file"),
- }
+	surat := &model.SuratKeluar{
+		Nomor:     r.FormValue("nomor"),
+		Tanggal:   r.FormValue("tanggal"),
+		Perihal:   r.FormValue("perihal"),
+		Ditujukan: r.FormValue("ditujukan"),
+		Title:     r.FormValue("title"),
+		File:      r.FormValue("existing_file"),
+	}
 
- var file io.Reader
- var fileName string
+	var file io.Reader
+	var fileName string
 
- fileUpload, fileHeader, err := r.FormFile("file")
- if err == nil {
-  file = fileUpload
-  fileName = fileHeader.Filename
-  defer fileUpload.Close()
- }
+	fileUpload, fileHeader, err := r.FormFile("file")
+	if err == nil {
+		file = fileUpload
+		fileName = fileHeader.Filename
+		defer fileUpload.Close()
+	}
 
- err = c.service.UpdateSuratKeluarByID(id, surat, file, fileName)
- if err != nil {
-  http.Error(w, "Gagal update data", http.StatusInternalServerError)
-  return
- }
+	err = c.service.UpdateSuratKeluarByID(id, surat, file, fileName)
+	if err != nil {
+		http.Error(w, "Gagal update data", http.StatusInternalServerError)
+		return
+	}
 
- json.NewEncoder(w).Encode(map[string]string{"message": "Surat keluar berhasil diupdate"})
+	json.NewEncoder(w).Encode(map[string]string{"message": "Surat keluar berhasil diupdate"})
 }
 
 func (c *SuratKeluarController) DeleteSuratKeluar(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
- id, _ := strconv.Atoi(ps.ByName("id"))
+	id, _ := strconv.Atoi(ps.ByName("id"))
 
- err := c.service.DeleteSuratKeluar(id)
- if err != nil {
-  http.Error(w, "Gagal menghapus data", http.StatusInternalServerError)
-  return
- }
+	err := c.service.DeleteSuratKeluar(id)
+	if err != nil {
+		http.Error(w, "Gagal menghapus data", http.StatusInternalServerError)
+		return
+	}
 
- json.NewEncoder(w).Encode(map[string]string{"message": "Surat keluar berhasil dihapus"})
-}
-
-func (c *SuratKeluarController) ServeFile(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-    fileName := ps.ByName("filename")
-    filePath := filepath.Join("static/suratkeluar", fileName)
-    if _, err := os.Stat(filePath); os.IsNotExist(err) {
-        http.Error(w, "File not found", http.StatusNotFound)
-        return
-    }
-    http.ServeFile(w, r, filePath)
+	json.NewEncoder(w).Encode(map[string]string{"message": "Surat keluar berhasil dihapus"})
 }
--- Sekretaris_Backend/go.mod
+++ Sekretaris_Backend/go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-playground/validator/v10 v10.25.0
 	github.com/go-sql-driver/mysql v1.9.0
+	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/rs/cors v1.11.1
@@ -19,7 +20,6 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/google/uuid v1.6.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	golang.org/x/net v0.34.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
